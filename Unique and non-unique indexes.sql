442a77964d2ead88a439c6216d7e12382af1c00f
Abraão Henrique Vasconcelos <henrique.vscncls@gmail.com>
2019-05-13 00:01:48 -0300

    Add files via upload

---
+++ Unique and non-unique indexes.sql
@@ -0,0 +1,7 @@
+/*
+Fonte: https://www.ibm.com/support/knowledgecenter/en/SSEPGG_10.5.0/com.ibm.db2.luw.admin.dbobj.doc/doc/c0061097.html
+
+* Unique and non-unique indexes
+* Including and excluding NULL keys
+* Differences between primary key or unique key constraints and unique indexes
+*/
